59d645b541572fa33fdd992ed54cffe5017ecce6
dark-life <jesus.da101@alu.ulpgc.es>
2016-05-24 15:29:10 +0100

    Completo

---
+++ heu.py
@@ -0,0 +1,80 @@
+from random import randint
+from utils import *
+import random 
+
+#aleatoria
+def heuristicRP(state):
+	if state.utility == 1:
+		return infinity
+	if state.utility == -1:
+		return -infinity
+
+	return randint(-2500, 2500)
+
+def heuristicRT(state):
+	return randint(-2500, 2500)
+
+def heuristicRIA(state):
+	if state.utility == 1:
+		return -infinity
+	if state.utility == -1:
+		return infinity
+
+	return randint(-2500, 2500)
+
+def heuristicN(state):
+	if state.utility == 1:
+		return infinity
+	if state.utility == -1:
+		return -infinity
+	return coste(state, 'X') -  coste(state, 'O')
+
+def coste(state, player):
+	"""If X wins with this move, return 1; if O return -1; else return 0"""
+	cost = 0
+	maxi = 0
+	list_legalmoves = legal_moves(state)
+	for move in list_legalmoves:
+		cost += k_in_row(state.board, move, player, (0, 1)) #arriba
+		cost += k_in_row(state.board, move, player, (1, 0)) #derecha
+		cost += k_in_row(state.board, move, player, (1, 1)) #arriba-der
+		cost += k_in_row(state.board, move, player, (1, -1)) #abajo-der
+		if cost > maxi:
+			maxi = cost
+	return maxi
+
+def legal_moves(state):
+        "Legal moves are any square not yet taken."
+        return [(x, y) for (x, y) in state.moves
+                if y == 1 or (x, y-1) in state.board]
+
+def k_in_row(board, move, player, (delta_x, delta_y)):
+	x, y = move
+	n = 0
+	ac = 0
+	while (board.get((x, y)) == player) or (board.get((x, y))  == None)  and x < 8 and y < 7:
+		if board.get((x, y))  == None:
+			n += 50
+		if board.get((x, y))  == player:
+			n += 100
+			ac += 1
+		x, y = x + delta_x, y + delta_y
+	x, y = move
+	while (board.get((x, y)) == player) or (board.get((x, y))  == None) and x > 0 and y > 0:
+		if board.get((x, y))  == None:
+			n += 50
+		if board.get((x, y))  == player:
+			n += 100
+			ac += 1
+		x, y = x - delta_x, y - delta_y
+	if ac == 3:
+		if player == 'X':
+			n *= 6
+		else:
+			n *= 12
+	if ac >= 4:
+		if player == 'X':
+			n *= 9
+		else:
+			n *= 18
+	return n
---
+++ run.py
@@ -0,0 +1,135 @@
+import games
+import heu
+
+def main():
+    print "Que desea hacer?"
+    print "1. Humano vs Humano.   ||   2. Humano vs IA.   ||   3. IA vs IA."
+    option = raw_input("Opcion: ")
+    if option == '1':
+        humanVsHuman()
+    else:
+        if option == '2':
+            humanVsMachine()
+        else:
+            print machineVsMachine()
+
+def selectPlayer():
+    print "Seleccione el jugador que va primero: "
+    print "1. X   ||   2. O"
+    player = raw_input("Jugador: ")
+    if player == '1':
+        return 'X'
+    else:
+        return 'O'
+
+def selectDifficulty(state, game):
+    print "Seleccione dificultad: "
+    print "1. Facil   ||   2. Medio   ||   3. Dificil"
+    dificultad = raw_input("Jugador: ")
+    if dificultad == '1':
+        return heu.heuristicRT
+    else:
+        if dificultad == '2':
+            return heu.heuristicRP
+        else:
+            return heu.heuristicN
+
+def humanVsHuman():
+    player = selectPlayer()
+    game = games.ConnectFour(h=7, v=6, k=4, player=player)
+    state = game.initial
+    while True:
+        print "Jugador a mover:", game.to_move(state)
+        game.display(state)
+        if player == 'O':
+            col_str = raw_input("Movimiento: ")
+            coor = int(str(col_str).strip())
+            x = coor
+            y = -1
+            legal_moves = game.legal_moves(state)
+            for lm in legal_moves:
+                if lm[0] == x:
+                    y = lm[1]
+            state = game.make_move((x, y), state)
+            player = 'X'
+        else:
+            col_str = raw_input("Movimiento: ")
+            coor = int(str(col_str).strip())
+            x = coor
+            y = -1
+            legal_moves = game.legal_moves(state)
+            for lm in legal_moves:
+                if lm[0] == x:
+                    y = lm[1]
+            state = game.make_move((x, y), state)
+            player = 'O'
+        print "-------------------"
+        if game.terminal_test(state):
+            game.display(state)
+            print "Final de la partida"
+            break
+
+def humanVsMachine():
+    player = selectPlayer()
+    game = games.ConnectFour(h=7, v=6, k=4, player=player)
+    state = game.initial
+    heu = selectDifficulty(state, game)
+    while True:
+        print "Jugador a mover:", game.to_move(state)
+        game.display(state)
+
+        if player == 'O':
+            x = checkValidMove()
+            y = -1
+            legal_moves = game.legal_moves(state)
+            for lm in legal_moves:
+                if lm[0] == x:
+                    y = lm[1]
+            state = game.make_move((x, y), state)
+            player = 'X'
+        else:
+            print "Thinking..."
+            move = games.alphabeta_search(state, game, d=5, cutoff_test=None, eval_fn=heu)
+            state = game.make_move(move, state)
+            player = 'O'
+        print "-------------------"
+        if game.terminal_test(state):
+            game.display(state)
+            print "Final de la partida"
+            break
+
+def machineVsMachine():
+    game = games.ConnectFour(h=7, v=6, k=4)
+    state = game.initial
+    player = 'X'
+    while True:
+        print "Jugador a mover:", game.to_move(state)
+        game.display(state)
+
+        if player == 'O':
+            print "Don't Think, Just ACT"
+            move = games.alphabeta_search(state, game, d=2, cutoff_test=None, eval_fn=heu.heuristicRIA)
+            state = game.make_move(move, state)
+            player = 'X'
+        else:
+            print "Thinking..."
+            move = games.alphabeta_search(state, game, d=3, cutoff_test=None, eval_fn=heu.heuristicN)
+            state = game.make_move(move, state)
+            player = 'O'
+        print "-------------------"
+        if game.terminal_test(state):
+            game.display(state)
+            print "Final de la partida"
+            break
+    
+
+def checkValidMove():
+    col_str = raw_input("Movimiento: ")
+    coor = int(str(col_str).strip())
+    while coor < 1 or coor > 7:
+        col_str = raw_input("Movimiento incorrecto, intentelo de nuevo: ")
+        coor = int(str(col_str).strip())
+    return coor
+
+if __name__ == '__main__':
+    main()
